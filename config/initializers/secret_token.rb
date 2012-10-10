6d8e0629aa4e8f32711ec99b9e641f573bd6614e
Tom Wilson <tom@jackhq.com>
2012-10-10 12:00:55 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quackernews::Application.config.secret_token = 'db1522402e0fcc5c5c355ef34e96da175fc73c7ba83e4db662468344c60562d74e896626e00cdb72691b6784817c410cc581f405ed4cae1109fe1b5566c3cc26'
